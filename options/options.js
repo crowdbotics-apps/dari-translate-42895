feeb7c2d33e9ca1554020b6cb43c7a8ba909dd51
Crowdbotics <team@crowdbotics.com>
2023-08-17 05:47:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dari-translate-42895",
+  url: "https://dari-translate-42895.botics.co",
+  api: "https://dari-translate-42895.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
